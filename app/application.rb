6996afeae11db7bcbee8a039421c50fe1ee19a09
Eva <yi.zheng1709@gmail.com>
2021-02-17 21:57:19 -0500

    commit tree e90b664bf893b8476d52ec5b544cdb53252372bd

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,13 @@
+require 'pry'
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
-    req = Rack::Request.new(env)
+    req = Rack::Request.new(env) 
+    #new instance of Request
 
     if req.path.match(/items/)
       @@items.each do |item|
@@ -13,6 +16,23 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else 
+        @@cart.each {|item| resp.write "#{item}\n"}
+      end 
+    elsif req.path.match(/add/)
+      wanted_item = req.params["item"]
+      #params is an attribute of the request object
+      #calling on the value of the 'item' key
+      if @@items.include?(wanted_item)
+        @@cart << wanted_item
+        resp.write "added #{wanted_item}"
+      else 
+        resp.write "We don't have that item"
+      end
+
     else
       resp.write "Path Not Found"
     end
